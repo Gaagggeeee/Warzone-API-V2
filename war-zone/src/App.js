09ce5754a01af8460cacfd7a2e5a936d07cf2ada
Gaagggeeee <rgeverett@miners.utep.edu>
2021-02-18 20:22:50 -0700

    update

--- war-zone/src/App.js
+++ war-zone/src/App.js
@@ -6,120 +6,108 @@ import Profile from './components/profile';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props) 
+  constructor(){
+    super();
     this.state = {
-        gamertag: '',
-        password: '',
-        wins: undefined,
-        kills: undefined,
-        deaths: undefined,
-        downs: undefined,
-        kdRatio: undefined,
-        revives: undefined,
-        topFive: undefined,
-        topTen: undefined,
-        topTwen: undefined,
-        gamesPlayed: undefined,
-        score: undefined,
-        SPM: undefined,
-        contracts: undefined,
-        cash: undefined,
-        timePlayed: undefined
-    }
+      gamertag: '',
+      platform: '',
+      wins: undefined,
+      kills: undefined
+    };
 
-    this.handleGamertag = this.handleGamertag.bind(this);
-    this.handleplatform = this.handlePlatform.bind(this);
+   
+
+  }
+
+  getStats = async (e) => {
+
+    e.preventDefault();
+
+    const gamertag = e.target.elements.gamertag.value; 
+    const platform = e.target.elements.platform.value;
+
+    const response = await fetch(`https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/${gamertag}/${platform}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "584a334868msh963be87d4acd8dep10151ejsn0bd0894e7aeb",
+        "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com"
+      }
+    });
     
-    this.getStat();
-}
+   const stats = await response.json();
+   console.log(stats);
+   
+   this.setState({
+      wins: stats.br.wins,
+      kills: stats.br.kills,
+      deaths:stats.br.deaths,
+      downs:stats.br.downs,
+      kdRatio:stats.br.kdRatio,
+      revives:stats.br.revives,
+      topFive:stats.br.topFive,
+      topTen:stats.br.topTen,
+      topTwen:stats.br.topTwentyFive,
+      gamesPlayed:stats.br.gamesPlayed,
+      score:stats.br.score,
+      SPM:stats.br.scorePerMinute,
+      contracts:stats.br.contracts,
+      cash:stats.br.cash,
+      timePlayed:stats.br.timePlayed
+   });
 
-  handleGamertag(event) {
-    this.setState({gamertag: event.target.value});
-    event.preventDefault();
-  }
-
-  handlePlatform(event) {
-    this.setState({platform: event.target.value});
-    event.preventDefault();
-  }
-
- 
+  };
   
-  getStat = async (event) => {
-    
-    const gamertag = 'gaagggeeee';
-    const platform = 'xbl';
-
-
-    if(gamertag && platform){
-
-
-      const response = await fetch(`https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/${gamertag}/${platform}`, {
-        method: 'GET',
-        headers: {
-          'x-rapidapi-key': '584a334868msh963be87d4acd8dep10151ejsn0bd0894e7aeb',
-          'x-rapidapi-host': 'call-of-duty-modern-warfare.p.rapidapi.com'
-        }
-      });
-      
-     
-
-        const stats = await response.json();
-
-        console.log(stats);
-
-        this.setState ({
-            wins:stats.wins,
-            kills:stats.kills,
-            deaths:stats.deaths,
-            downs:stats.downs,
-            kdRatio:stats.kdRatio,
-            revives:stats.revives,
-            topFive:stats.topFive,
-            topTen:stats.topTen,
-            topTwen:stats.topTwen,
-            gamesPlayed:stats.gamesPlayed,
-            score:stats.score,
-            SPM:stats.SPM,
-            contracts:stats.contracts,
-            cash:stats.cash,
-            timePlayed:stats.timePlayed
-        });
-    } else {
-        this.setState({error: true});
-    }
-}
 
   render(){
     return (
       <>
-                  <Container>
-                    <Row>
-                        <Col>
-                            <form onSubmit={this.getStat}>
-                                <input 
-                                    type='text'
-                                    value={this.gamertag}
-                                    onChange={this.GamertagChange}
-                                    autoComplete='off'
-                                    placeholder='Enter Gamertag'
-                                />
-                                <input 
-                                    type='text'
-                                    value={this.platform}
-                                    onChange={this.PlatformChange}
-                                    autoComplete='off'
-                                    placeholder='Enter Platform'
-                                />
-                                <input 
-                                    type='submit'
-                                />
-                            </form>
-                        </Col>
-                    </Row>
-                </Container>
-                <Profile />
+        <Container className='form-container' fluid={true} >
+          <Row>
+            <Col>
+              <div className='form-box'>
+                <form onSubmit={this.getStats}>
+                  <input 
+                    className='input-box'
+                    type='text'
+                    name='gamertag'
+                    autoComplete='off'
+                    placeholder='Enter Gamertag'
+                  />
+                  <input 
+                    className='input-box'
+                    type='text'
+                    name='platform'
+                    autoComplete='off'
+                    placeholder='Enter Platform'
+                  />
+                  <div>
+                  <input 
+                    className='input-box'
+                    type='submit'
+                  />
+                  </div>
+                </form>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        <Profile 
+          wins={this.state.wins}
+          kills={this.state.kills}
+          deaths={this.state.deaths}
+          downs={this.state.downs}
+          kdRatio={this.state.kdRatio}
+          revives={this.state.revives}
+          topFive={this.state.topFive}
+          topTen={this.state.topTen}
+          topTwen={this.state.topTwen}
+          gamesPlayed={this.state.gamesPlayed}
+          score={this.state.score}
+          SPM={this.state.SPM}
+          contracts={this.state.contracts}
+          cash={this.state.cash}
+          timePlayed={this.state.timePlayed}
+        />
       </>
     );
   }
--- war-zone/src/components/profile.js
+++ war-zone/src/components/profile.js
@@ -8,87 +8,104 @@ const Profile = props => {
 
     return(
         <>
-            <Container className='profileContainer'>
+            <Container className='profile-container' fluid={true} >
 {/* FIRST CONTENT ROW */}
-                <Row className='contentRow'>
+                <Row className='content-row'>
                     <Col sm='2'>
-                        <div className='contentBox'> 
-                            <p>Wins: {props.wins} </p>
+                        <div className='content-box'>
+                            <div className='content-inner-box'> 
+                                <p className='stat-name'>Wins:</p>
+                                <p className='prop-box'>{props.wins}</p>
+                            </div>
                         </div>
                     </Col>
                     <Col sm='2'>
-                        <div className='contentBox'> 
-                            <p>Kills: {props.kills}</p> 
+                        <div className='content-box'> 
+                            <p>Kills:</p>
+                            <p className='prop-box'>{props.kills}</p> 
                         </div>
                     </Col>
                     <Col sm='2'>
-                        <div className='contentBox'> 
-                            <p>Deaths: {props.deaths}</p> 
+                        <div className='content-box'> 
+                            <p>Deaths:</p>
+                            <p className='prop-box'>{props.deaths}</p> 
                         </div>
                     </Col>
                     <Col sm='2'>
-                        <div className='contentBox'> 
-                            <p>Downs: {props.downs} </p>
+                        <div className='content-box'> 
+                            <p>Downs:</p>
+                            <p className='prop-box'>{props.downs}</p>
                         </div>
                     </Col>
                     <Col sm='2'>
-                        <div className='contentBox'>
-                            <p>KD: {props.kdRatio} </p>
+                        <div className='content-box'>
+                            <p>KD:</p>
+                            <p className='prop-box'>{props.kdRatio}</p>
                         </div>
                     </Col>
                     <Col sm='2'>
-                        <div className='contentBox'>
-                            <p>Revives: {props.revives}</p>
+                        <div className='content-box'>
+                            <p>Revives:</p>
+                            <p className='prop-box'>{props.revives}</p>
                         </div>
                     </Col>
                 </Row>
 {/* CONTENT ROW TWO */}
                 <Row className='contentRow'>
                     <Col sm='2'>
-                        <div className='contentBox'> 
-                            <p>top Five: {props.topFive} </p>
+                        <div className='content-box'> 
+                            <p>top Five:</p>
+                            <p className='prop-box'>{props.topFive}</p>
                         </div>
                     </Col>
                     <Col sm='2'>
-                        <div className='contentBox'> 
-                            <p>Top Ten: {props.topTen}</p> 
+                        <div className='content-box'> 
+                            <p>Top Ten:</p>
+                            <p className='prop-box'>{props.topTen}</p> 
                         </div>
                     </Col>
                     <Col sm='2'>
-                        <div className='contentBox'> 
-                            <p>Top TwenyFive: {props.topTwen}</p> 
+                        <div className='content-box'> 
+                            <p>Top TwenyFive:</p>
+                            <p className='prop-box'>{props.topTwen}</p> 
                         </div>
                     </Col>
                     <Col sm='2'>
-                        <div className='contentBox'> 
-                            <p>Games Played: {props.gamesPlayed} </p>
+                        <div className='content-box'> 
+                            <p>Games Played:</p>
+                            <p className='prop-box'>{props.gamesPlayed}</p>
                         </div>
                     </Col>
                     <Col sm='2'>
-                        <div className='contentBox'>
-                            <p>Score: {props.score} </p>
+                        <div className='content-box'>
+                            <p>Score:</p>
+                            <p className='prop-box'>{props.score}</p>
                         </div>
                     </Col>
                     <Col sm='2'>
-                        <div className='contentBox'>
-                            <p>Score Per Minute: {props.SPM}</p>
+                        <div className='content-box'>
+                            <p>Score Per Minute:</p>
+                            <p className='prop-box'>{props.SPM}</p>
                         </div>
                     </Col>
                 </Row>
                 <Row>
                     <Col sm='4'>
-                        <div className='contentBox'>
-                            <p>Contracts: {props.contracts}</p>
+                        <div className='content-box'>
+                            <p>Contracts:</p>
+                            <p className='prop-box'>{props.contracts}</p>
                         </div>
                     </Col>
                     <Col sm='4'>
-                        <div className='contentBox'>
-                            <p>Cash: {props.cash}</p>
+                        <div className='content-box'>
+                            <p>Cash:</p>
+                            <p className='prop-box'>{props.cash}</p>
                         </div>
                     </Col>
                     <Col sm='4'>
-                        <div className='contentBox'>
-                            <p>Time Played: {props.timePlayed}</p>
+                        <div className='content-box'>
+                            <p>Time Played:</p>
+                            <p className='prop-box'>{props.timePlayed}</p>
                         </div>
                     </Col>
                 </Row>
